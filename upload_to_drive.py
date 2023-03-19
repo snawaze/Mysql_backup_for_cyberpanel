b94645910d949991c9b796ef2ddd33f20f171a21
Hiru556 <76238759+damith555@users.noreply.github.com>
2023-03-19 20:36:09 +0530

    Create upload_to_drive.py

---
+++ upload_to_drive.py
@@ -0,0 +1,55 @@
+import os
+import pickle
+import sys
+import time
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from googleapiclient.errors import HttpError
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/drive']
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    return creds
+
+def upload_to_drive(file_path):
+    """Uploads a file to Google Drive.
+
+    Args:
+        file_path: The path to the file to upload.
+    """
+    # Authenticate and create the Drive API client.
+    creds = get_credentials()
+    service = build('drive', 'v3', credentials=creds)
+
+    # Create a Media
