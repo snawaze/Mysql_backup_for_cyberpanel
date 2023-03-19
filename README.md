030bb211dc0aac61cbc7a1ad5d717e55b10497c0
Hiru556 <76238759+damith555@users.noreply.github.com>
2023-03-19 12:23:10 +0530

    Create a folder for each backup

--- README.md
+++ README.md
@@ -17,3 +17,5 @@ Mysql backup script for use in cyberpanel with gzip compression
 ###########################################################
 
 This script retrieves the MySQL password from CyberPanel using the cat command and stores it in the mysql_password variable. Then it uses this password to connect to the MySQL server and back up each database separately. Note that you should make sure that the mysql command is in the system's PATH or provide the full path to the command if it's not.
+
+This script uses the mysql command to get the list of databases, and then iterates over the list to backup each database separately. The resulting backup file for each database is saved in the specified backup directory with a filename that includes the database name and the current date and time.
--- mysql_backup.sh
+++ mysql_backup.sh
@@ -2,14 +2,14 @@
 
 ###########################################################
 # Script: mysql_backup.sh
-# Description: Backup all MySQL databases separately
+# Description: Backup all MySQL databases to a single compressed file
 # Author: Your Name
 # Date: March 25, 2023
 # Usage: ./mysql_backup.sh
 ###########################################################
 
 # Set backup directory
-backup_dir="/home/backup"
+backup_dir="/home/backup/$(date +%Y-%m-%d_%H-%M-%S)"
 
 # Get MySQL password from CyberPanel
 mysql_password=$(cat /etc/cyberpanel/mysqlPassword)
@@ -17,8 +17,14 @@ mysql_password=$(cat /etc/cyberpanel/mysqlPassword)
 # Get list of databases
 databases=$(mysql -u root -p"$mysql_password" -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)")
 
+# Create backup directory
+mkdir -p "$backup_dir"
+
 # Backup each database separately
 for db in $databases; do
-  backup_file="$backup_dir/$db-$(date +%Y-%m-%d_%H-%M-%S).sql.gz"
-  /usr/bin/mysqldump -h localhost -u root -p"$mysql_password" "$db" | gzip -9 -c > "$backup_file"
+  backup_file="$backup_dir/$db.sql.gz"
+  /usr/bin/mysqldump -h localhost -u root -p"$mysql_password" "$db" | gzip -c > "$backup_file"
 done
+
+# Compress all backup files into a single archive
+tar -czvf "$backup_dir/all_databases-$(date +%Y-%m-%d_%H-%M-%S).tar.gz" "$backup_dir"
