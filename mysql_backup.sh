e2f86f7da110ace597e978d4c97048f761108061
Hiru556 <76238759+damith555@users.noreply.github.com>
2023-03-19 12:48:56 +0530

    compress all
    compress all individiual dbs to a one tar file inside the folder

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -3,22 +3,25 @@
 ###########################################################
 # Script: mysql_backup.sh
 # Description: Backup all MySQL databases to a single compressed file
-# Author: Your Name
+# Author: Hirantha Bandara Muramudali
 # Date: March 25, 2023
 # Usage: ./mysql_backup.sh
 ###########################################################
 
+#set directory name using timestamp
+timestamp_for_bkp=$(date +%Y-%m-%d_%H-%M-%S)
+
 # Set backup directory
-backup_dir="/home/backup/$(date +%Y-%m-%d_%H-%M-%S)"
+backup_dir="/home/backup/${timestamp_for_bkp}"
+
+# Create backup directory
+mkdir -p "$backup_dir"
 
 # Get MySQL password from CyberPanel
 mysql_password=$(cat /etc/cyberpanel/mysqlPassword)
 
-# Get list of databases
-databases=$(mysql -u root -p"$mysql_password" -e "SHOW DATABASES;" | grep -Ev "(Database|information_schema|performance_schema)")
-
-# Create backup directory
-mkdir -p "$backup_dir"
+# Get list of databases (excluding system databases)
+databases=$(mysql -u root -p"$mysql_password" -e "SHOW DATABASES;" | grep -Ev "(Database|mysql|performance_schema|information_schema)")
 
 # Backup each database separately
 for db in $databases; do
@@ -27,4 +30,7 @@ for db in $databases; do
 done
 
 # Compress all backup files into a single archive
-tar -czvf "$backup_dir/all_databases-$(date +%Y-%m-%d_%H-%M-%S).tar.gz" "$backup_dir"
+tar -czvf "${backup_dir}/all_databases-${timestamp_for_bkp}.tar.gz" "$backup_dir"
+
+# Remove individual backup files
+#rm -f "${backup_dir}"/*.sql.gz
