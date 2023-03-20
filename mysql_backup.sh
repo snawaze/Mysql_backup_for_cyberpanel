c5ae25651c65153d80b4256cf057936518e78d1a
Hiru556 <76238759+damith555@users.noreply.github.com>
2023-03-20 22:06:52 +0530

    Update mysql_backup.sh

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -9,9 +9,9 @@
 # Usage: ./mysql_backup.sh
 ###########################################################
 
-#set directory name using timestamp
-
+# Set directory name using timestamp
 timestamp_for_bkp=$(date +%Y-%m-%d__%H-%M)
+backup_dir="/home/backup/"
 
 # Get MySQL password from CyberPanel
 mysql_password=$(cat /etc/cyberpanel/mysqlPassword)
@@ -20,59 +20,43 @@ mysql_password=$(cat /etc/cyberpanel/mysqlPassword)
 databases=$(mysql -u root -p"$mysql_password" -e "SHOW DATABASES;" | grep -Ev "(Database|mysql|performance_schema|information_schema)")
 
 # Create backup directory
-if mkdir -p "$timestamp_for_bkp"; then
-  echo "$(date) - Backup directory created: $timestamp_for_bkp" >> backup.log
+if mkdir -p "${backup_dir}${timestamp_for_bkp}"; then
+  echo "$(date) - Backup directory created: ${backup_dir}${timestamp_for_bkp}" >> "${backup_dir}backup.log"
 else
-  echo "$(date) - Error creating backup directory: $timestamp_for_bkp" >> backup_error_log
+  echo "$(date) - Error creating backup directory: ${backup_dir}${timestamp_for_bkp}" >> "${backup_dir}backup_error_log"
   exit 1
 fi
 
 # Backup each database separately
 for db in $databases; do
-  backup_file="$timestamp_for_bkp/$db.sql.gz"
-  if /usr/bin/mysqldump -h localhost -u root -p"$mysql_password" "$db" | gzip -c > "$backup_file"; then
-    echo "$(date) - Database backup completed: $backup_file" >> backup.log
+  backup_file="${backup_dir}${timestamp_for_bkp}/${db}.sql.gz"
+  if /usr/bin/mysqldump -h localhost -u root -p"$mysql_password" "$db" | gzip -5 -c > "${backup_file}"; then
+    echo "$(date) - Database backup completed: ${backup_file}" >> "${backup_dir}backup.log"
   else
-    echo "$(date) - Error backing up database: $db" >> backup_error_log
+    echo "$(date) - Error backing up database: $db" >> "${backup_dir}backup_error_log"
     exit 1
   fi
 done
 
 # Compress all backup files into a single archive
-if tar -czvf "all_databases-${timestamp_for_bkp}.tar.gz" "$timestamp_for_bkp"; then
-  echo "$(date) - Backup files compressed into: all_databases-${timestamp_for_bkp}.tar.gz" >> backup.log
+if tar -czvf "${backup_dir}all_databases-${timestamp_for_bkp}.tar.gz" "${backup_dir}${timestamp_for_bkp}"; then
+  echo "$(date) - Backup files compressed into: ${backup_dir}all_databases-${timestamp_for_bkp}.tar.gz" >> "${backup_dir}backup.log"
 else
-  echo "$(date) - Error compressing backup files into: all_databases-${timestamp_for_bkp}.tar.gz" >> backup_error_log
+  echo "$(date) - Error compressing backup files into: ${backup_dir}all_databases-${timestamp_for_bkp}.tar.gz" >> "${backup_dir}backup_error_log"
   exit 1
 fi
 
-
-
-
-# Upload the backup file to Mega.nz
-# /usr/local/megatools/megatools put  /home/backup/mysql_backup.sh
-if /home/backup/megatools/megatools put  all_databases-${timestamp_for_bkp}.tar.gz  --path /Trash; then
-  echo "$(date) - Backup file uploaded to Mega.nz: all_databases-${timestamp_for_bkp}.tar.gz" >> backup.log
+# Upload the backup file to Mega.nz  Putting in the Trash folder as the file needs to be deleted after 30days inorder to prevent over usage
+if "${backup_dir}"megatools/megatools put "${backup_dir}all_databases-${timestamp_for_bkp}.tar.gz" --path /Trash; then
+  echo "$(date) - Backup file uploaded to Mega.nz: ${backup_dir}all_databases-${timestamp_for_bkp}.tar.gz" >> "${backup_dir}backup.log"
 else
-  echo "$(date) - Error uploading backup file to Mega.nz: all_databases-${timestamp_for_bkp}.tar.gz" >> backup_error_log
+  echo "$(date) - Error uploading backup file to Mega.nz: ${backup_dir}all_databases-${timestamp_for_bkp}.tar.gz" >> "${backup_dir}backup_error_log"
   #exit 1
 fi
 
 # # Upload the backup file to Google Drive using the Drive API
 # # Replace <path_to_credentials_file> with the path to your Google Drive API credentials file
-# if python3 upload_to_drive.py "<path_to_credentials_file>" "all_databases-${timestamp_for_bkp}.tar.gz"; then
-#   echo "$(date) - Backup file uploaded to Google Drive: all_databases-${timestamp_for_bkp}.tar.gz" >> backup.log
+# if python3 upload_to_drive.py "<path_to_credentials_file>" "${backup_dir}all_databases-${timestamp_for_bkp}.tar.gz"; then
+#   echo "$(date) - Backup file uploaded to Google Drive: ${backup_dir}all_databases-${timestamp_for_bkp}.tar.gz" >> ${backup_dir}backup.log
 # else
-#   echo "$(date) - Error uploading backup file to Google Drive: all_databases-${timestamp_for_bkp}.tar.gz" >> backup_error_log
-#   exit 1
-# fi
-
-# Remove individual backup files
-if rm -r "$timestamp_for_bkp"; then
-  echo "$(date) - Individual backup files removed: $timestamp_for_bkp" >> backup.log
-else
-  echo "$(date) - Error removing individual backup files: $timestamp_for_bkp" >> backup_error_log
-  exit 1
-fi
-# Log time taken for backup process
-echo "$(date) - Backup process completed in $(printf '%02d:%02d' $((SECONDS/60)) $((SECONDS%60))) minutes:seconds to file all_databases-${timestamp_for_bkp}.tar.gz" >> backup.log
+#   echo "$(date) - Error uploading backup file to Google Drive: ${backup_dir}all_databases-${timestamp_for
